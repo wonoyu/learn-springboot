23dfa9d69aaa63e11575793c9a105eefd2c1816f
ahmadayuwono <ahmadarys@gmail.com>
2023-04-05 22:41:45 +0700

    tugas hello world

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    String hello() {
-        return "Hello World!";
+    public String hello() {
+        return "Hello, World!";
     }
 }
